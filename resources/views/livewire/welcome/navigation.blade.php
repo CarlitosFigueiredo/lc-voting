34059383be6c8e0d965e2ec6e20259f7c8bb4a59
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2023-12-11 10:54:10 -0300

    Inicio, setup e configurações

---
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -0,0 +1,11 @@
+<div class="px-6 py-4">
+    @auth
+        <a href="{{ url('/index') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Dashboard</a>
+    @else
+        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Log in</a>
+
+        @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ms-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Register</a>
+        @endif
+    @endauth
+</div>
