88061ece036a026fcee834db31c024a9338c9512
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2023-12-11 14:37:42 -0300

    Layouts, embaçado aqui

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index')->name('index');;
+Route::view('/', 'index')->name('index');
+
+Route::view('/idea', 'show');
 
 // Route::view('dashboard', 'dashboard')
 //     ->middleware(['auth', 'verified'])
